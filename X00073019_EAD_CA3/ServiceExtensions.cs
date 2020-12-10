b84d45bf5ed992d76eccb8326fcef85f6fc8926c
James Lynam <drachion@msn.com>
2020-12-10 15:21:53 +0000

    Fixed deployment issues

--- X00073019_EAD_CA3/ServiceExtensions.cs
+++ X00073019_EAD_CA3/ServiceExtensions.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Security.Claims;
-using System.Text;
+﻿using System.Net.Http;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace EADCA3
 {
